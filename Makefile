2a7d67822ae867d0bdd3644bbae35c7e109af181
Edan <edan.landow@gmail.com>
2023-06-13 23:23:08 +1000

    conversion to useful datapoints complete, updated makefile, bugfix where fortran was adding unneccesary spaces to csv

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
-.PHONY: gendata
+.PHONY: gendata csvconversion todatapoints
 
 gendata:
 	gcc c/generate_data.c -o c/compiled_c -lm
 	c/compiled_c
+
+csvconversion:
+	gfortran -o fortran/compiled fortran/convert_to_csv.f95
+	fortran/compiled
+
+todatapoints:
+	gcc -o cpp/compiled_cpp cpp/refine_datapoints.cpp -lstdc++
+	cpp/compiled_cpp
--- fortran/convert_to_csv.f95
+++ fortran/convert_to_csv.f95
@@ -24,14 +24,14 @@ implicit none
 
     open(1, file="data/output.demo", status="old")
     open(2, file="data/output.csv", status="new")
-    write(2, *) "time, direction, amplitude"
+    write(2, *) "time,direction,amplitude"
     do while (read_eof)
         read(1, '(a)', iostat = eof_error) io_buffer
         if (eof_error == 0) then
             if (io_buffer == "time") then
                 value_target = 1
                 if (.not. skip_first_instance) then
-                    print_row = trim(print_time)//", "//trim(print_direction)//", "//trim(print_amplitude)
+                    print_row = trim(print_time)//","//trim(print_direction)//","//trim(print_amplitude)
                     write(2, *) trim(print_row)
                 end if
                 print_time = "undefined"
@@ -57,7 +57,7 @@ implicit none
         else if (eof_error == -1) then ! -1 thrown if end of file, -2 thrown for other read errors
             read_eof = .FALSE.
             if (.not. skip_first_instance) then
-                print_row = trim(print_time)//", "//trim(print_direction)//", "//trim(print_amplitude)
+                print_row = trim(print_time)//","//trim(print_direction)//","//trim(print_amplitude)
                 write(2, *) trim(print_row)
             end if
         else if (eof_error == -2) then
